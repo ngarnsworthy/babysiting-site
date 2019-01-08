c32057bed109dc79dd081bc6eb817bb0bc249cf4
ngarnsworthy <ngarnsworthy@gmail.com>
2019-01-08 08:52:53 -0600

    today

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ http.createServer(function(req, res) {
         fs.readFile(filePath, function(error, content) {
           if (error) {
             if (error.code == 'ENOENT') {
-              fs.readFile('./404.html', function(error, content) {
+              fs.readFile('404.html', function(error, content) {
                 res.writeHead(200, {
                   'Content-Type': contentType
                 });
